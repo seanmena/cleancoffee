4652c7c2dd59cac06ef4632fdd38965e3b5a4273
Sean Mena <seanmena13@gmail.com>
2022-10-08 13:18:35 -0600

    some foundations

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cleancoffee
+
 Clean coffee its freaky fresh
+
+# Goal
+
+Create a webpage with HTML, CSS, jQuery, and Bulma that is organized efficient and working.
+
+# Design
+
+Created rough mock up of design in Adobe Illustrator (attached below)
+![pic of template](./pics/clean%20coffee.jpg)
---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("this is touching");
+
+aboutMe = $("#about-notif");
+aboutMe.hide();
+
+$("#about-me").on("click", function () {});
+
+// Check for click events on the navbar burger icon
+$(".navbar-burger").click(function () {
+  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  $(".navbar-burger").toggleClass("is-active");
+  $(".navbar-menu").toggleClass("is-active");
+});
+
+$("#about-x").on("click", function () {
+  aboutMe.hide();
+  $("#hero-title").show();
+});
